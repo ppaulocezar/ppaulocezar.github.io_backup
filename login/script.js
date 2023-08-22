15e5e0ec10ae9bf4490ab81a80bfa9d70fc81581
ppaulocezar <125910249+ppaulocezar@users.noreply.github.com>
2023-08-22 18:10:07 -0300

    Update script.js

--- login/script.js
+++ login/script.js
@@ -3,6 +3,8 @@ function checkCode() {
   const correctCode = "codigo123"; // Substitua pelo código correto
 
   if (codeInput === correctCode) {
+    // Define um cookie ou variável de sessão para indicar que o código está correto
+    document.cookie = "accessGranted=true";
     window.location.href = "index2.html";
   } else {
     const errorMessage = document.getElementById("errorMessage");
